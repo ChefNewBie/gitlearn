b439f2c1f19db6cf9613dd8ec8fef100996904bb
ChefNewBie <jack.sahiwal@gmail.com>
2019-12-15 16:56:28 -0500

    Calculator Function

---
+++ calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+#Testing integration
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def sub(num1, num2):
+    return num1-num2
+
+
+def main():
+    print ("Hello World!")
+    print ("Calling Addittion")
+    a = add(2, 5)
+    b = sub(19, 10)
+    print("Addittion=%d" % a)
+    print("Subtraction=%d" % b)
+
+
+main()
+# if __name__ == "__main__":
+
